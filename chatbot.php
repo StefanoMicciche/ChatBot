a8cddda163c9285f80c7d392066167fa553884f9
Stefano <miccichestefano@gmail.com>
2025-04-03 16:18:50 +0200

    feat(bot):chatbot init

--- chatbot.php
+++ chatbot.php
@@ -18,4 +18,21 @@ class Chatbot {
                 'max_lenghth' => 200,
                 'temperature' => 0.5
             ]])};
+
+    $ch = curl_init($this->url);
+    curl_setopt_array($ch, [
+
+        CURLOPT_RETURNTRANSFER => true,
+        CURL_OPT_POST => true,
+        CURLOPT_POSTFIELDS => $data,
+        CURLOPT_HTTPHEADER => [
+            'Authorization: Bearer '. $this->token,
+            'Content-Type: application/json'
+        ]
+        ]);
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return json_decode($response, true);
 ?>
--- chatbotConfig.php
+++ chatbotConfig.php
@@ -1,4 +1,3 @@
 <?php
-define('API_TOKEN', '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' )
-define('API_URL', 'https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill'. API_TOKEN);
+define('API_TOKEN', getenv('API_TOKEN'));
 ?>
